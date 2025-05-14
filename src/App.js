86f5c5cb8d6f4ac679ff0e6645f1d4d361f10f80
Binu Peter <binupeterp07@gmail.com>
2025-05-14 18:22:41 +0100

    Add navbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import "./App.css";
 
 const App = () => {
   return (
-    <>
+    <div className="grid-layout">
       <Nav />
-      <Header />
-      <Footer />
-    </>
+    </div>
   );
 };
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/Logo.svg";
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+    console.log("clicked");
+  };
+
   return (
-    <nav>
-      <div>
-        <img src={Logo} alt="Little lemon logo" />
+    <nav className={`navbar ${menuOpen? "open": ""}`}>
+      <a href="/" className="logo">
+        <img src={Logo} alt="Little Lemon Restaurant Logo" />
+      </a>
+
+      {/* Hamburger */}
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
-      <div>
-        <ul>
-          <li>
-            <a href="/">Home</a></li>
-           <li> <a href="/">About</a></li>
-           <li> <a href="/">Menu</a></li>
-           <li> <a href="/">Order Online</a></li>
-           <li> <a href="/">Reservations</a></li>
-           <li> <a href="/">Login</a>
-          </li>
-        </ul>
-      </div>
+
+      <ul className={` nav-links ${menuOpen ? "visible" : ""}`}>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Services</a>
+        </li>
+        <li>
+          <a href="/">Menu</a>
+        </li>
+        <li>
+          <a href="/">Reservations</a>
+        </li>
+        <li>
+          <a href="/">Order Online</a>
+        </li>
+        <li>
+          <a href="/">Login</a>
+        </li>
+      </ul>
     </nav>
   );
 };
